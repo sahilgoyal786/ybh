1740e419d14b00c75a6dd87f6808a7badce37128
Dev <dev@32bitsolutions.com>
2020-10-24 13:58:19 +0530

    common styles & endpoints added

---
+++ src/common/styles.js
@@ -0,0 +1,13 @@
+
+ const styles = {
+    error_message: {
+      fontSize: 10,
+      color: 'red',
+      width: '100%',
+      marginTop: 5,
+      marginBottom: 10,
+    }
+};
+
+
+export default styles ;
---
+++ src/components/apis/endPoints.js
@@ -0,0 +1,9 @@
+
+ const EndiPoints = {
+
+forgotPassword:"forgot-password",
+
+};
+
+export default EndiPoints
+
